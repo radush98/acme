479e2380caad5eaf6244c396212d95a4446ed406
radush-v <vladymyr.r@radency.com>
2026-07-09 22:12:04 +0300

    feat: added button component

--- acme/app/page.tsx
+++ acme/app/page.tsx
@@ -4,7 +4,6 @@ import { Logo } from "@/components/main/logo/Logo";
 import { Sidebar } from "@/components/main/sidebar/Sidebar";
 import { SidebarItem } from "@/components/main/sidebar/sidebarItem/SidebarItem";
 import { SIDEBAR_ITEMS } from "@/shared/constants/siderbarItems";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -23,24 +22,5 @@ export default function Home() {
         </Content>
       </div>
     </div>
-
-    // <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 px-6 py-16 font-sans dark:bg-black">
-    //   <main className="w-full max-w-2xl rounded-2xl bg-white p-10 shadow-sm dark:bg-zinc-950">
-    //     <h1 className="text-3xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-50">
-    //       Главная страница
-    //     </h1>
-
-    //     <p className="mt-4 text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-    //       Добро пожаловать. Перейдите на страницу файла по ссылке ниже.
-    //     </p>
-
-    //     <Link
-    //       href="/file/example"
-    //       className="mt-8 inline-flex h-11 items-center justify-center rounded-full bg-zinc-900 px-6 text-sm font-medium text-white transition-colors hover:bg-zinc-700 dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-300"
-    //     >
-    //       Открыть /file/example
-    //     </Link>
-    //   </main>
-    // </div>
   );
 }
---
+++ acme/components/shared/Button.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx"
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+    primary?: boolean;
+    secondary?: boolean;
+    danger?: boolean;
+    onClick?: () => void;
+    disabled?: boolean;
+}
+
+export const Button: React.FC<ButtonProps> = ({ children, primary, secondary, danger, onClick, disabled, ...props }) => {
+    return <button
+        className={clsx({
+            'bg-primary text-white': primary,
+            'bg-secondary text-white': secondary,
+            'bg-danger text-white': danger,
+        }, 'px-4 py-2 rounded-md hover:cursor-pointer')}
+        onClick={onClick}
+        disabled={disabled}
+        {...props}>
+        {children}
+    </button>
+}
